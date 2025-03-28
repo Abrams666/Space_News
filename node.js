f60f0683c1e6f47b5df92515410582c50f1f170d
Abrams <yuanchianchen@gmail.com>
2025-03-28 14:06:49 +0800

    init

---
+++ node.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	//res.setHeader("Content-Type", "text/html");
+	res.writeHead(200);
+	res.end("hello world");
+});
+
+app.listen(80, () => {
+	console.log(`Server running on port 80`);
+});
